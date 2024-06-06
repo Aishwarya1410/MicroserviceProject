3d3fc5d467d97f410528e7d31ab690247a3c7cc3
Aishwarya1410 <aishwaryasmaa@gmail.com>
2024-06-06 21:55:31 +0530

    Initial Commit

---
+++ banking-core-service-apigateway/src/main/java/com/microservice/finance/banking_core_service_apigateway/BankingCoreServiceApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.finance.banking_core_service_apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingCoreServiceApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingCoreServiceApigatewayApplication.class, args);
+	}
+
+}
---
+++ banking-core-service-apigateway/src/test/java/com/microservice/finance/banking_core_service_apigateway/BankingCoreServiceApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.finance.banking_core_service_apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingCoreServiceApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
